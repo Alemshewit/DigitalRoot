0c559cea608cfe50b21a1bc0c162b7be41296e5c
Alemshewit <alemshewitweldearegay@gmail.com>
2014-09-09 12:10:31 -0600

    New changes to Digital
    Added a for loop and it works for any input given!

--- DigitalRoot/DigitalRoot/Program.cs
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -11,7 +11,9 @@ namespace DigitalRoot
         static void Main(string[] args)
         {
             //call up the function DigitalRoot with a string parameter and print the value to the console.
-            Console.WriteLine(DigitalRoot("3425"));
+            Console.WriteLine(DigitalRoot("31337"));
+            Console.WriteLine(DigitalRoot("45734"));
+
             //keep console open
             Console.ReadKey();
         }
@@ -19,9 +21,20 @@ namespace DigitalRoot
         //create a function DigitalRoot that takes an argument and gives the digital root value of it.
         static int DigitalRoot(string rootThis)
         {
-            //take the string argument, convert to char array, add them up, convert the x to 
+            //declare a variable to hold the sum of the string after we loop through it in the for loop
+            int rootSum = 0;
+            //run a for loop through the string until all characters have gone through the loop
+            for (int i = 0; i < rootThis.Length; i++)
+            {
+                //take each character in the index and convert them to strings and then into integers
+                //and add them to the variable rootSum each time the loop is excuted
+                rootSum += int.Parse(rootThis[i].ToString());
+
+            }
+
+            //take the rootSum and convert it to strings before we can get the sum, add them up, convert the x to 
             //string and then to an integer and store in an int variable input
-            int input = rootThis.ToCharArray().Sum(x => int.Parse(x.ToString()));
+            int input = rootSum.ToString().Sum(x => int.Parse(x.ToString()));
             //return the value of input
             return input;
         }
